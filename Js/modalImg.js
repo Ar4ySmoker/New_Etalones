8421ae9d01345023ebc83857ec8c045bc5c82202
Artur Fedcenco <developerar4y@gmail.com>
2023-08-21 16:21:40 +0300

    stopWorkMap(noResultMedia)

--- Js/modalImg.js
+++ Js/modalImg.js
@@ -1,3 +1,4 @@
+console.log("hello2");
 $(document).ready(function () {
   $("#passport").click(() => {
     $(".modalImgP").fadeIn(700);
@@ -36,6 +37,20 @@ $(document).ready(function () {
   $("#closePS").click(() => {
     $(".modalImgPS").fadeOut(500, function () {
       $(".modalImgPS").css("display", "none");
+      $("body").css("overflow-y", "auto");
+    });
+  });
+});
+$(document).ready(function () {
+  $("#check").click(() => {
+    $(".modalImgCK").fadeIn(700);
+    $(".modalImgCK").css("display", "flex");
+    $("body").css("overflow-y", "hidden");
+  });
+
+  $("#closeCK").click(() => {
+    $(".modalImgCK").fadeOut(500, function () {
+      $(".modalImgCK").css("display", "none");
       $("body").css("overflow-y", "auto");
     });
   });
